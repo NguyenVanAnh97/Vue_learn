9fbf492b9e639ca7f4bdfbc8cf766e3cbcd15f32
vace <nguyenvananh270897@gmail.com>
2024-04-20 13:41:31 +0700

    import boostrap framework css

--- my_project/src/main.js
+++ my_project/src/main.js
@@ -6,5 +6,9 @@ import { createApp } from 'vue'
 /*import Event_vue3 from './Event_vue3.vue' mouse_event*/
 import Render_vue3 from './Render_vue.vue'
 
-createApp(Render_vue3).mount('#app')
+/* import boostrap */
+import  'bootstrap/dist/css/bootstrap.css' 
+import  'bootstrap-vue/dist /bootstrap-vue.css' 
+
+createApp(Render_vue3).use(BootstrapVue).mount('#app')
 
