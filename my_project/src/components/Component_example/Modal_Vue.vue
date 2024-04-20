321b3e173ad33ad97139cbac87060887ed4bbdd4
vace <nguyenvananh270897@gmail.com>
2024-04-20 14:08:33 +0700

    update

---
+++ my_project/src/components/Component_example/Modal_Vue.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div>
+            <b-button>Button</b-button>
+            <b-button variant="danger">Button</b-button>
+            <b-button variant="success">Button</b-button>
+            <b-button variant="outline-primary">Button</b-button>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+
+
+        return {}
+    }
+}
+</script>
+
+<style lang="css" scoped></style>
--- my_project/src/main.js
+++ my_project/src/main.js
@@ -4,11 +4,16 @@ import { createApp } from 'vue'
 /* import App from './App.vue' systax_vue3*/
 /* import SystaxVue from './Systax_V3.vue' isModal, template*/
 /*import Event_vue3 from './Event_vue3.vue' mouse_event*/
-import Render_vue3 from './Render_vue.vue'
+/*import Render_vue3 from './Render_vue.vue' render Vue3*/
+import ModelViewBoostrap from './components/Component_example/Modal_Vue.vue'
+
 
 /* import boostrap */
-import  'bootstrap/dist/css/bootstrap.css' 
-import  'bootstrap-vue/dist /bootstrap-vue.css' 
 
-createApp(Render_vue3).use(BootstrapVue).mount('#app')
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+
+
+
+createApp(ModelViewBoostrap).mount('#app')
 
