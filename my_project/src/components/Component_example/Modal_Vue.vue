4cc9214b2ae3b1b58d8b087b694ba80162e14a0c
vace <nguyenvananh270897@gmail.com>
2024-04-20 14:44:39 +0700

    update

--- my_project/src/components/Component_example/Modal_Vue.vue
+++ my_project/src/components/Component_example/Modal_Vue.vue
@@ -1,39 +1,19 @@
 <template>
     <div>
-        <b-button size="sm" @click="toggle">
-            {{ show ? 'Hide' : 'Show' }} Alert
-        </b-button>
-        <b-alert v-model="show" class="mt-3" dismissible @dismissed="dismissed">
-            Hello {{ name }}!
-        </b-alert>
+
     </div>
 </template>
 
 <script>
 export default {
-    data() {
-    return {
-      name: 'BootstrapVue',
-      show: true
-    }
-  },
-  watch: {
-    show(newVal) {
-      console.log('Alert is now ' + (newVal ? 'visible' : 'hidden'))
-    }
-  },
-  methods: {
-    toggle() {
-      console.log('Toggle button clicked')
-      this.show = !this.show
-    },
-    dismissed() {
-      console.log('Alert dismissed')
-    }
-  },
+    setup () {
+        
 
-  compatConfig: { MODE: 3 }
+        return {}
+    }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
